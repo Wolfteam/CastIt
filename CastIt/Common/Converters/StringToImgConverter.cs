feef7a9bad31dceafcca93504bd86bf04d382f09
Efrain Bastidas <mimo4325@gmail.com>
2020-05-04 19:26:52 -0500

    Added a string to img converter to load the thumbnail of the current file

---
+++ CastIt/Common/Converters/StringToImgConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CastIt.Common.Converters
+{
+    public class StringToImgConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bm = LoadImage(value as string);
+            if (bm is null)
+            {
+                var img = System.Windows.Application.Current.Resources["NoImgFound"] as ImageSource;
+                return img;
+            }
+            return ConvertBitmapToBitmapImage(bm);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Bitmap LoadImage(string path)
+        {
+            if (string.IsNullOrEmpty(path) || !File.Exists(path))
+            {
+                return null;
+            }
+            var ms = new MemoryStream(File.ReadAllBytes(path));
+            return (Bitmap)Image.FromStream(ms);
+        }
+
+        public static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Bmp);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
+        }
+    }
+}
