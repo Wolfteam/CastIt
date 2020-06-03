022985a525a6e5c43697512e0563c06bbb93789b
Efrain Bastidas <mimo4325@gmail.com>
2020-06-03 18:28:17 -0500

    [CastIt] Moved the youtube code to the main app

---
+++ CastIt/Models/Youtube/YoutubeMedia.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CastIt.Models.Youtube
+{
+    public class YoutubeMedia
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string Url { get; set; }
+        public int SelectedQuality { get; set; }
+        public List<int> Qualities { get; set; }
+            = new List<int>();
+    }
+}
