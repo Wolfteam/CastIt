58e41b30f6b9c04ccc8a7078b428455e2c4264d5
Efrain Bastidas <mimo4325@gmail.com>
2020-06-03 18:27:24 -0500

    [CastIt.ConsoleApp] Added an example for subtitles

--- CastIt.GoogleCast.ConsoleApp/Program.cs
+++ CastIt.GoogleCast.ConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using CastIt.GoogleCast;
+using CastIt.GoogleCast.Enums;
 using CastIt.GoogleCast.Models.Media;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,17 +36,48 @@ namespace CastIt.ConsoleApp
 
             player.Init();
             await player.ConnectAsync();
+            bool canSeek = false;
+
             //await player.LoadAsync(new MediaInformation
             //{
             //    ContentId = @"http://192.168.1.101:9696/videos?seconds=0&file=F:\Videos\Da Capo Opening 1.mp4",
             //    Duration = 90
             //});
 
-            await player.LoadAsync(new MediaInformation
+            var mediaInfo = new MediaInformation
             {
-                ContentId = "https://www.youtube.com/watch?v=O2mEccTaxiw",
-                ContentType = "video/webm"
-            });
+                ContentId = @"http://192.168.1.101:9696/videos?seconds=600&file=F:\Movies\John%20Wick\John%20Wick%203%20Parabellum.mkv",
+                ContentType = "video/mp4",
+                StreamType = StreamType.Live,
+                Metadata = new MovieMetadata
+                {
+                    Title = "John wick",
+                },
+                Tracks = new List<Track>
+                {
+                    new Track
+                    {
+                        TrackId = 1,
+                        SubType = TextTrackType.Subtitles,
+                        Type = TrackType.Text,
+                        Name = "English",
+                        TrackContentId = "http://192.168.1.101:9696/subtitles/subs.vtt",
+                        Language = "en-US"
+                    }
+                },
+                TextTrackStyle = new TextTrackStyle
+                {
+                    BackgroundColor = Color.Transparent,
+                    EdgeColor = Color.Black,
+                    FontScale = 1.2F,
+                    WindowType = TextTrackWindowType.Normal,
+                    EdgeType = TextTrackEdgeType.None,
+                    FontStyle = TextTrackFontStyleType.Normal,
+                    FontGenericFamily = TextTrackFontGenericFamilyType.Casual,
+                }
+            };
+
+            await player.LoadAsync(mediaInfo, true, 0, 360, 1);
 
             Console.WriteLine("Tap to pause");
             Console.ReadKey();
@@ -53,12 +87,16 @@ namespace CastIt.ConsoleApp
             Console.ReadKey();
             await player.PlayAsync();
 
-            Console.WriteLine("Tap to seek 30 seconds");
-            Console.ReadKey();
-            await player.SeekAsync(30);
+            if (canSeek)
+            {
+                Console.WriteLine("Tap to seek 30 seconds");
+                Console.ReadKey();
+                await player.SeekAsync(30);
+            }
 
             Console.WriteLine("Type any key to disconnect");
             Console.ReadKey();
+            await player.StopPlaybackAsync();
             player.Dispose();
 
             Console.WriteLine("Type any key to close this app");
