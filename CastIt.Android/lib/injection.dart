6d808a13a67c0993832e5ba576f55ccee28d68bd
Efrain Bastidas <mimo4325@gmail.com>
2020-07-30 21:59:26 -0500

    [CastIt.Android] Updated some libs

--- CastIt.Android/lib/injection.dart
+++ CastIt.Android/lib/injection.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
 import 'package:log_4_dart_2/log_4_dart_2.dart';
 
 import 'services/logging_service.dart';
@@ -7,7 +6,6 @@ import 'services/settings_service.dart';
 
 final GetIt getIt = GetIt.instance;
 
-@injectableInit
 void initInjection() {
   getIt.registerSingleton(Logger());
   getIt.registerSingleton<LoggingService>(LoggingServiceImpl(getIt<Logger>()));
