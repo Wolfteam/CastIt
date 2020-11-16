63bbeb80a38f5576ae6e6289b4f160593f3ec0e3
Efrain Bastidas <mimo4325@gmail.com>
2020-11-15 20:56:16 -0500

    [Domain] Added some exceptions

---
+++ CastIt.Domain/Exceptions/ConnectingException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CastIt.Domain.Exceptions
+{
+    public class ConnectingException : Exception
+    {
+        public ConnectingException() : base()
+        {
+        }
+
+        public ConnectingException(string message) : base(message)
+        {
+        }
+
+        public ConnectingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
