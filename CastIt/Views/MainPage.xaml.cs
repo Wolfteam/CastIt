a51296d2f09b219512371909b8c5946182df732a
Efrain <mimo4325@gmail.com>
2021-04-07 21:33:22 -0500

    [CastIt] Crash fix

--- CastIt/Views/MainPage.xaml.cs
+++ CastIt/Views/MainPage.xaml.cs
@@ -122,18 +122,21 @@ namespace CastIt.Views
         {
             if (e.Value == null)
                 return;
-            var tabs = GetFinalPlayListsPositions();
-            //If we are in tab mode
-            if (tabs.Count > 0)
+            await Dispatcher.InvokeAsync(async () =>
             {
-                var (playlist, logicalIndex) = tabs.FirstOrDefault(t => t.Key.Id == e.Value.Id);
-                await ViewModel.DeletePlayList(logicalIndex, playlist);
-            }
-            //else we are in grid view mode
-            else
-            {
-                await ViewModel.DeletePlayList(ViewModel.PlayLists.IndexOf(e.Value), e.Value);
-            }
+                var tabs = GetFinalPlayListsPositions();
+                //If we are in tab mode
+                if (tabs.Count > 0)
+                {
+                    var (playlist, logicalIndex) = tabs.FirstOrDefault(t => t.Key.Id == e.Value.Id);
+                    await ViewModel.DeletePlayList(logicalIndex, playlist);
+                }
+                //else we are in grid view mode
+                else
+                {
+                    await ViewModel.DeletePlayList(ViewModel.PlayLists.IndexOf(e.Value), e.Value);
+                }
+            });
         }
     }
 }
