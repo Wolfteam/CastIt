07963359c7acd68f4331ce73030b7ed4f72097fa
Efrain <mimo4325@gmail.com>
2021-05-19 20:27:10 -0500

    Updated utils

--- CastIt.Android/lib/common/extensions/string_extensions.dart
+++ CastIt.Android/lib/common/extensions/string_extensions.dart
@@ -1,12 +1,12 @@
-extension StringExtensions on String {
+extension StringExtensions on String? {
   /// Returns true if string is:
   /// - null
   /// - empty
   /// - whitespace string.
   ///
   /// Characters considered "whitespace" are listed [here](https://stackoverflow.com/a/59826129/10830091).
-  bool get isNullEmptyOrWhitespace => this == null || isEmpty || trim().isEmpty;
+  bool get isNullEmptyOrWhitespace => this == null || this!.isEmpty || this!.trim().isEmpty;
 
   bool isLengthValid({int minLength = 0, int maxLength = 255}) =>
-      this == null || trim().isEmpty || length > maxLength || length < minLength;
+      this == null || this!.trim().isEmpty || this!.length > maxLength || this!.length < minLength;
 }
