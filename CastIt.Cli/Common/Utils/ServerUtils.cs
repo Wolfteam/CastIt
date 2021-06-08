28f3abc2c5b570980c54978223cf995a1fa82a20
Efrain <mimo4325@gmail.com>
2021-06-07 21:51:03 -0500

    [CastIt.Cli] Added some exceptions

--- CastIt.Cli/Common/Utils/ServerUtils.cs
+++ CastIt.Cli/Common/Utils/ServerUtils.cs
@@ -1,44 +1,57 @@
-﻿using System.IO;
-using CastIt.Application.Server;
+﻿using CastIt.Application.Server;
 using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CastIt.Cli.Common.Utils
 {
     public static class ServerUtils
     {
-        public static string StartServerIfNotStarted(IConsole console, string ffmpegPath = null, string ffprobePath = null)
+        public static string StartServerIfNotStarted(string ffmpegPath = null, string ffprobePath = null)
         {
             if (WebServerUtils.IsServerAlive())
             {
-                console.WriteLine("Server is alive, getting its port...");
+                Console.WriteLine("Server is alive, getting its port...");
                 var port = WebServerUtils.GetServerPort();
                 if (port.HasValue)
                     return WebServerUtils.GetWebServerIpAddress(port.Value);
-                console.WriteLine("Couldn't retrieve the server port");
+                Console.WriteLine("Couldn't retrieve the server port");
                 return null;
             }
+            Console.WriteLine("Web server is not running, starting it...");
 
             var dir = Directory.GetCurrentDirectory();
             var path = Path.Combine(dir, "Server", WebServerUtils.ServerProcessName);
-            console.WriteLine($"Trying to start process located in = {path}");
-//#if DEBUG
-//            path = "E:\\Proyectos\\CastIt\\CastIt.Server\\bin\\Debug\\net5.0\\CastIt.Server.exe";
-//#endif
-
-            console.WriteLine("Web server is not running, starting it...");
+#if DEBUG
+            path = "D:\\Proyectos\\CastIt\\CastIt.Test\\bin\\Debug\\net5.0\\CastIt.Test.exe";
+#endif
+            Console.WriteLine("Getting an open port...");
             var openPort = WebServerUtils.GetOpenPort();
-            var args = new[]
+            var args = new List<string>
             {
-                AppWebServerConstants.PortArgument, $"{openPort}",
-                AppWebServerConstants.FFmpegPathArgument, ffmpegPath,
-                AppWebServerConstants.FFprobePathArgument, ffprobePath
+                AppWebServerConstants.PortArgument, $"{openPort}"
             };
+
+            if (!string.IsNullOrWhiteSpace(ffmpegPath))
+            {
+                args.Add(AppWebServerConstants.FFmpegPathArgument);
+                args.Add(ffmpegPath);
+            }
+
+            if (!string.IsNullOrWhiteSpace(ffprobePath))
+            {
+                args.Add(AppWebServerConstants.FFprobePathArgument);
+                args.Add(ffprobePath);
+            }
+
             var escapedArgs = ArgumentEscaper.EscapeAndConcatenate(args);
+            Console.WriteLine($"Trying to start process located in = {path} with args = {escapedArgs}");
             bool started = WebServerUtils.StartServer(escapedArgs, path);
             if (started)
                 return WebServerUtils.GetWebServerIpAddress(openPort);
 
-            console.WriteLine("Couldn't start the web server");
+            Console.WriteLine("Couldn't start the web server");
             return null;
         }
     }
