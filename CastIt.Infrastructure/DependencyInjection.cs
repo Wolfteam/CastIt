d01932ba7fb818cbb610631696c3537c3b2ca712
Efrain <mimo4325@gmail.com>
2021-06-09 14:41:57 -0500

    [WIP] [CastIt.Infrastructure] Removed not needed services

--- CastIt.Infrastructure/DependencyInjection.cs
+++ CastIt.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,6 @@
 ﻿using CastIt.Domain.Models.Logging;
 using CastIt.GoogleCast;
 using CastIt.GoogleCast.Interfaces;
-using CastIt.Infrastructure.Interfaces;
-using CastIt.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
@@ -13,16 +11,17 @@ namespace CastIt.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddSingleton<IAppSettingsService, AppSettingsService>();
+            //services.AddSingleton<IAppSettingsService, AppSettingsService>();
+            //TODO: MOVE THIS TO THE GOOGLE CAST PROJECT
             services.AddSingleton<IPlayer>(provider => new Player(provider.GetRequiredService<ILogger<Player>>()));
-            services.AddSingleton<ICastService, CastService>();
+            //services.AddSingleton<ICastService, CastService>();
 
             return services;
         }
 
         public static IServiceCollection AddServerInfrastructure(this IServiceCollection services)
         {
-            services.AddSingleton<IAppSettingsService, AppSettingsService>();
+            //services.AddSingleton<IAppSettingsService, AppSettingsService>();
             services.AddSingleton<IPlayer>(provider => new Player(provider.GetRequiredService<ILogger<Player>>()));
             return services;
         }
@@ -31,8 +30,8 @@ namespace CastIt.Infrastructure
         {
             return new List<FileToLog>
             {
-                new FileToLog(typeof(CastService), "service_cast"),
-                new FileToLog(typeof(AppSettingsService), "service_app_settings")
+                //new FileToLog(typeof(CastService), "service_cast"),
+                //new FileToLog(typeof(AppSettingsService), "service_app_settings")
             };
         }
     }
---
+++ CastIt.Infrastructure/Models/ServerAppSettings.cs
@@ -0,0 +1,52 @@
+﻿using CastIt.Domain.Enums;
+using CastIt.GoogleCast.Enums;
+
+namespace CastIt.Test.Models
+{
+    public class ServerAppSettings
+    {
+        public string FFmpegPath { get; set; }
+        public string FFprobePath { get; set; }
+
+        public string CurrentAppMigration { get; set; }
+        public bool StartFilesFromTheStart { get; set; }
+        public bool PlayNextFileAutomatically { get; set; }
+        public bool ForceVideoTranscode { get; set; }
+        public bool ForceAudioTranscode { get; set; }
+        public VideoScaleType VideoScale { get; set; }
+        public bool EnableHardwareAcceleration { get; set; }
+
+        public SubtitleFgColorType CurrentSubtitleFgColor { get; set; }
+        public SubtitleBgColorType CurrentSubtitleBgColor { get; set; }
+        public SubtitleFontScaleType CurrentSubtitleFontScale { get; set; }
+        public TextTrackFontStyleType CurrentSubtitleFontStyle { get; set; }
+        public TextTrackFontGenericFamilyType CurrentSubtitleFontFamily { get; set; }
+        public double SubtitleDelayInSeconds { get; set; }
+        public bool LoadFirstSubtitleFoundAutomatically { get; set; }
+
+        public ServerAppSettings Copy()
+        {
+            return new ServerAppSettings
+            {
+                FFmpegPath = FFmpegPath,
+                FFprobePath = FFprobePath,
+
+                CurrentAppMigration = CurrentAppMigration,
+                StartFilesFromTheStart = StartFilesFromTheStart,
+                PlayNextFileAutomatically = PlayNextFileAutomatically,
+                ForceVideoTranscode = ForceVideoTranscode,
+                ForceAudioTranscode = ForceAudioTranscode,
+                VideoScale = VideoScale,
+                EnableHardwareAcceleration = EnableHardwareAcceleration,
+
+                CurrentSubtitleFgColor = CurrentSubtitleFgColor,
+                CurrentSubtitleBgColor = CurrentSubtitleBgColor,
+                CurrentSubtitleFontScale = CurrentSubtitleFontScale,
+                CurrentSubtitleFontStyle = CurrentSubtitleFontStyle,
+                CurrentSubtitleFontFamily = CurrentSubtitleFontFamily,
+                SubtitleDelayInSeconds = SubtitleDelayInSeconds,
+                LoadFirstSubtitleFoundAutomatically = LoadFirstSubtitleFoundAutomatically
+            };
+        }
+    }
+}
