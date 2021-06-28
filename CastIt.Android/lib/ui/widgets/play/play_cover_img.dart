a3f13ce677450228425e60b24a069275ed0d019e
Efrain <mimo4325@gmail.com>
2021-06-28 00:02:31 -0500

    [WIP] [CastIt.Android] Updated some parts of the ui

--- CastIt.Android/lib/ui/widgets/play/play_cover_img.dart
+++ CastIt.Android/lib/ui/widgets/play/play_cover_img.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-';
+
 import '../../../bloc/server_ws/server_ws_bloc.dart';
 import '../../../common/extensions/string_extensions.dart';
 import '../../../common/styles.dart';
