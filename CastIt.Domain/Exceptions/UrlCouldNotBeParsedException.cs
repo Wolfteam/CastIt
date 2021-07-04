7bfcf0e54d8adc4394107b351a962965bb08c339
Efrain <mimo4325@gmail.com>
2021-07-03 21:12:07 -0500

    [CastIt.Application] Added some new exceptions Removed not needed code

---
+++ CastIt.Domain/Exceptions/UrlCouldNotBeParsedException.cs
@@ -0,0 +1,12 @@
+﻿using CastIt.Domain.Enums;
+
+namespace CastIt.Domain.Exceptions
+{
+    public class UrlCouldNotBeParsedException : BaseAppException
+    {
+        public UrlCouldNotBeParsedException(string url, AppMessageType errorMessageId = AppMessageType.UrlCouldntBeParsed)
+            : base($"The {url} could not be parsed", errorMessageId)
+        {
+        }
+    }
+}
