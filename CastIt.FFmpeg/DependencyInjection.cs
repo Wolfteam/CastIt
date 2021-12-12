798bdf881b3735b2e5dce2027f5f2b1a9bff8ab3
Efrain <mimo4325@gmail.com>
2021-12-11 22:13:25 -0500

    Moved some logic to a ffmpeg project

---
+++ CastIt.FFmpeg/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CastIt.Domain.Models.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace CastIt.FFmpeg
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddFFmpeg(this IServiceCollection services)
+        {
+            services.AddSingleton<IFFmpegService, FFmpegService>();
+            return services;
+        }
+
+        public static List<FileToLog> AddFFmpegLogs(this List<FileToLog> logs)
+        {
+            logs.AddRange(new List<FileToLog>
+            {
+                new FileToLog(typeof(FFmpegService), "service_ffmpeg"),
+            });
+            return logs;
+        }
+    }
+}
---
+++ CastIt.FFmpeg/IFFmpegService.cs
@@ -0,0 +1,60 @@
+﻿using CastIt.Domain.Enums;
+using CastIt.Domain.Models.FFmpeg.Info;
+using CastIt.Domain.Models.FFmpeg.Transcode;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CastIt.FFmpeg
+{
+    public interface IFFmpegService
+    {
+        CancellationTokenSource TokenSource { get; }
+
+        Task Init(string ffmpegExePath, string ffprobeExePath);
+
+        void RefreshFfmpegPath(string ffmpegExePath, string ffprobeExePath);
+
+        string GetThumbnail(string mrl);
+
+        Task GenerateThumbnails(string mrl, bool hwAccelIsEnabled);
+
+        Task KillThumbnailProcess();
+
+        Task KillTranscodeProcess();
+
+        Task<Stream> TranscodeVideo(TranscodeVideoFile options);
+
+        Task<MemoryStream> TranscodeMusic(TranscodeMusicFile options);
+
+        Task<FFProbeFileInfo> GetFileInfo(string filePath, CancellationToken token);
+
+        Task GenerateSubTitles(
+            string filePath,
+            string subtitleFinalPath,
+            double seconds,
+            int index,
+            double subsDelayInSeconds,
+            CancellationToken token);
+
+        bool VideoNeedsTranscode(
+            int videoStreamIndex,
+            bool forceVideoTranscode,
+            VideoScaleType selectedScale,
+            FFProbeFileInfo fileInfo);
+
+        bool AudioNeedsTranscode(
+            int audioStreamIndex,
+            bool forceAudioTranscode,
+            FFProbeFileInfo fileInfo,
+            bool checkIfAudioIsNull = false);
+
+        HwAccelDeviceType GetHwAccelToUse(
+            int videoStreamIndex,
+            FFProbeFileInfo fileInfo,
+            bool shouldUseHwAccel = true,
+            bool isHls = false);
+
+        Task<byte[]> GetThumbnailTile(string mrl, long tentativeSecond, int fps);
+    }
+}
