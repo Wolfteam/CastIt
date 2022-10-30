ff2ea2d783ee824f58abd2d7bba6cc2ac6f289e5
Efrain Bastidas <mimo4325@gmail.com>
2022-10-30 18:16:49 -0500

    Updated some pacakges

--- CastIt.Android/macos/Flutter/GeneratedPluginRegistrant.swift
+++ CastIt.Android/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,9 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus_macos
-import network_info_plus_macos
-import package_info_plus_macos
+import device_info_plus
+import network_info_plus
+import package_info_plus
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
--- CastIt.Android/windows/flutter/generated_plugins.cmake
+++ CastIt.Android/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  network_info_plus_windows
+  network_info_plus
   url_launcher_windows
   window_size
 )
