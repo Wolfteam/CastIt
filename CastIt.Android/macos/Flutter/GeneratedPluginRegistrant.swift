46bcebdcdf5bc266d2c4f8749c525d29cd6fe644
Efrain Bastidas <mimo4325@gmail.com>
2023-05-13 21:18:51 -0500

    [Android] Updated flutter project

--- CastIt.Android/macos/Flutter/GeneratedPluginRegistrant.swift
+++ CastIt.Android/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,8 @@ import Foundation
 import device_info_plus
 import network_info_plus
 import package_info_plus
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import window_size
--- CastIt.Android/windows/flutter/generated_plugin_registrant.cc
+++ CastIt.Android/windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <network_info_plus/network_info_plus_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_size/window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
   WindowSizePluginRegisterWithRegistrar(
