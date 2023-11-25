b21d1a6e3ce4cab29ef94308596d0477dfd7f4d2
Efrain Bastidas <mimo4325@gmail.com>
2023-11-24 23:55:52 -0500

    Updated to flutter 3.16 Updated all packages

--- CastIt.Client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ CastIt.Client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,7 @@ import window_size
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
